9cdc72c95cb09700bda15a5ddef0d0bb67ee4c86
Ingvar Stepanyan <me@rreverser.com>
2023-04-05 20:58:48 +0100

    Remove AlpacaState wrapper

--- src/alpaca_state.rs
+++ src/alpaca_state.rs
@@ -6,16 +6,9 @@ use ascom_alpaca::api::{
 };
 use ascom_alpaca::{ASCOMError, ASCOMErrorCode, ASCOMResult};
 use chrono::{DateTime, Utc};
-use std::sync::atomic::AtomicU32;
-
-#[derive(Debug)]
-pub struct AlpacaState {
-    pub sa: StarAdventurer,
-    pub sti: AtomicU32,
-}
 
 #[async_trait::async_trait]
-impl Device for AlpacaState {
+impl Device for StarAdventurer {
     fn static_name(&self) -> &str {
         "StarAdventurer"
     }
@@ -32,11 +25,11 @@ impl Device for AlpacaState {
     ) -> ascom_alpaca::ASCOMResult<String> {
         match &*action {
             "pending_declination_slew" => {
-                let change = self.sa.get_pending_dec_change().await;
+                let change = self.get_pending_dec_change().await;
                 Ok(change.to_string())
             }
             "complete_declination_slew" => {
-                self.sa.complete_dec_slew().await;
+                self.complete_dec_slew().await;
                 Ok("".to_string())
             }
             "set_pier_side_after_manual_move" => {
@@ -50,7 +43,7 @@ impl Device for AlpacaState {
                         ))
                     }
                 };
-                self.sa.set_pier_side_after_manual_move(pier_side).await;
+                self.set_pier_side_after_manual_move(pier_side).await;
                 Ok("".to_string())
             }
             _ => Err(ASCOMError::new(
@@ -84,16 +77,16 @@ impl Device for AlpacaState {
 
     /* Connected */
     async fn connected(&self) -> ASCOMResult<bool> {
-        Ok(self.sa.is_connected().await)
+        Ok(self.is_connected().await)
     }
 
     async fn set_connected(&self, connected: bool) -> ASCOMResult<()> {
         if connected {
             tracing::warn!("Connecting");
-            self.sa.connect().await
+            self.connect().await
         } else {
             tracing::warn!("Disconnecting");
-            self.sa.disconnect().await
+            self.disconnect().await
         }
     }
 
@@ -123,209 +116,208 @@ impl Device for AlpacaState {
 }
 
 #[async_trait::async_trait]
-impl Telescope for AlpacaState {
+impl Telescope for StarAdventurer {
     async fn alignment_mode(&self) -> ASCOMResult<AlignmentModeResponse> {
-        self.sa.get_alignment_mode().await
+        self.get_alignment_mode().await
     }
 
     async fn altitude(&self) -> ASCOMResult<f64> {
-        self.sa.get_altitude().await
+        self.get_altitude().await
     }
 
     async fn aperture_area(&self) -> ASCOMResult<f64> {
-        self.sa.get_aperture_area().await
+        self.get_aperture_area().await
     }
 
     async fn aperture_diameter(&self) -> ASCOMResult<f64> {
-        self.sa.get_aperture().await
+        self.get_aperture().await
     }
 
     async fn at_home(&self) -> ASCOMResult<bool> {
-        self.sa.is_home().await
+        self.is_home().await
     }
 
     async fn at_park(&self) -> ASCOMResult<bool> {
-        self.sa.is_parked().await
+        self.is_parked().await
     }
 
     async fn azimuth(&self) -> ASCOMResult<f64> {
-        self.sa.get_azimuth().await
+        self.get_azimuth().await
     }
 
     async fn can_find_home(&self) -> ASCOMResult<bool> {
-        self.sa.can_find_home().await
+        self.can_find_home().await
     }
 
     async fn can_park(&self) -> ASCOMResult<bool> {
-        self.sa.can_park().await
+        self.can_park().await
     }
 
     async fn can_pulse_guide(&self) -> ASCOMResult<bool> {
-        self.sa.can_pulse_guide().await
+        self.can_pulse_guide().await
     }
 
     async fn can_set_declination_rate(&self) -> ASCOMResult<bool> {
-        self.sa.can_set_declination_rate().await
+        self.can_set_declination_rate().await
     }
 
     async fn can_set_guide_rates(&self) -> ASCOMResult<bool> {
-        self.sa.can_set_guide_rates().await
+        self.can_set_guide_rates().await
     }
 
     async fn can_set_park(&self) -> ASCOMResult<bool> {
-        self.sa.can_set_park_pos().await
+        self.can_set_park_pos().await
     }
 
     async fn can_set_pier_side(&self) -> ASCOMResult<bool> {
-        self.sa.can_set_side_of_pier().await
+        self.can_set_side_of_pier().await
     }
 
     async fn can_set_right_ascension_rate(&self) -> ASCOMResult<bool> {
-        self.sa.can_set_ra_rate().await
+        self.can_set_ra_rate().await
     }
 
     async fn can_set_tracking(&self) -> ASCOMResult<bool> {
-        self.sa.can_set_tracking().await
+        self.can_set_tracking().await
     }
 
     async fn can_slew(&self) -> ASCOMResult<bool> {
-        self.sa.can_slew().await
+        self.can_slew().await
     }
 
     async fn can_slew_alt_az(&self) -> ASCOMResult<bool> {
-        self.sa.can_slew_alt_az().await
+        self.can_slew_alt_az().await
     }
 
     async fn can_slew_alt_az_async(&self) -> ASCOMResult<bool> {
-        self.sa.can_slew_alt_az_async().await
+        self.can_slew_alt_az_async().await
     }
 
     async fn can_slew_async(&self) -> ASCOMResult<bool> {
-        self.sa.can_slew_async().await
+        self.can_slew_async().await
     }
 
     async fn can_sync(&self) -> ASCOMResult<bool> {
-        self.sa.can_sync().await
+        self.can_sync().await
     }
 
     async fn can_sync_alt_az(&self) -> ASCOMResult<bool> {
-        self.sa.can_sync_alt_az().await
+        self.can_sync_alt_az().await
     }
 
     async fn can_unpark(&self) -> ASCOMResult<bool> {
-        self.sa.can_unpark().await
+        self.can_unpark().await
     }
 
     async fn declination(&self) -> ASCOMResult<f64> {
-        self.sa.get_dec().await
+        self.get_dec().await
     }
 
     async fn declination_rate(&self) -> ASCOMResult<f64> {
-        self.sa.get_declination_rate().await
+        self.get_declination_rate().await
     }
 
     async fn set_declination_rate(&self, declination_rate: f64) -> ASCOMResult<()> {
-        self.sa.set_declination_rate(declination_rate).await
+        self.set_declination_rate(declination_rate).await
     }
 
     async fn does_refraction(&self) -> ASCOMResult<bool> {
-        self.sa.does_refraction().await
+        self.does_refraction().await
     }
 
     async fn set_does_refraction(&self, does_refraction: bool) -> ASCOMResult<()> {
-        self.sa.set_does_refraction(does_refraction).await
+        self.set_does_refraction(does_refraction).await
     }
 
     async fn equatorial_system(&self) -> ASCOMResult<EquatorialSystemResponse> {
-        self.sa.get_equatorial_system().await
+        self.get_equatorial_system().await
     }
 
     async fn focal_length(&self) -> ASCOMResult<f64> {
-        self.sa.get_focal_length().await
+        self.get_focal_length().await
     }
 
     async fn guide_rate_declination(&self) -> ASCOMResult<f64> {
-        self.sa.get_guide_rate_declination().await
+        self.get_guide_rate_declination().await
     }
 
     async fn set_guide_rate_declination(&self, guide_rate_declination: f64) -> ASCOMResult<()> {
-        self.sa
-            .set_guide_rate_declination(guide_rate_declination)
+        self.set_guide_rate_declination(guide_rate_declination)
             .await
     }
 
     async fn guide_rate_right_ascension(&self) -> ASCOMResult<f64> {
-        self.sa.get_guide_rate_ra().await
+        self.get_guide_rate_ra().await
     }
 
     async fn set_guide_rate_right_ascension(
         &self,
         guide_rate_right_ascension: f64,
     ) -> ASCOMResult<()> {
-        self.sa.set_guide_rate_ra(guide_rate_right_ascension).await
+        self.set_guide_rate_ra(guide_rate_right_ascension).await
     }
 
     async fn is_pulse_guiding(&self) -> ASCOMResult<bool> {
-        self.sa.is_pulse_guiding().await
+        self.is_pulse_guiding().await
     }
 
     async fn right_ascension(&self) -> ASCOMResult<f64> {
-        self.sa.get_ra().await
+        self.get_ra().await
     }
 
     async fn right_ascension_rate(&self) -> ASCOMResult<f64> {
-        self.sa.get_ra_rate().await
+        self.get_ra_rate().await
     }
 
     async fn set_right_ascension_rate(&self, right_ascension_rate: f64) -> ASCOMResult<()> {
-        self.sa.set_ra_rate(right_ascension_rate).await
+        self.set_ra_rate(right_ascension_rate).await
     }
 
     async fn side_of_pier(&self) -> ASCOMResult<SideOfPierResponse> {
-        self.sa.get_side_of_pier().await
+        self.get_side_of_pier().await
     }
 
     async fn set_side_of_pier(
         &self,
         side_of_pier: TelescopeSetSideOfPierRequestSideOfPier,
     ) -> ASCOMResult<()> {
-        self.sa.set_side_of_pier(side_of_pier).await
+        self.set_side_of_pier(side_of_pier).await
     }
 
     async fn sidereal_time(&self) -> ASCOMResult<f64> {
-        self.sa.get_sidereal_time().await
+        self.get_sidereal_time().await
     }
 
     async fn site_elevation(&self) -> ASCOMResult<f64> {
-        self.sa.get_elevation().await
+        self.get_elevation().await
     }
 
     async fn set_site_elevation(&self, site_elevation: f64) -> ASCOMResult<()> {
-        self.sa.set_elevation(site_elevation).await
+        self.set_elevation(site_elevation).await
     }
 
     async fn site_latitude(&self) -> ASCOMResult<f64> {
-        self.sa.get_latitude().await
+        self.get_latitude().await
     }
 
     async fn set_site_latitude(&self, site_latitude: f64) -> ASCOMResult<()> {
-        self.sa.set_latitude(site_latitude).await
+        self.set_latitude(site_latitude).await
     }
 
     async fn site_longitude(&self) -> ASCOMResult<f64> {
-        self.sa.get_longitude().await
+        self.get_longitude().await
     }
 
     async fn set_site_longitude(&self, site_longitude: f64) -> ASCOMResult<()> {
-        self.sa.set_longitude(site_longitude).await
+        self.set_longitude(site_longitude).await
     }
 
     async fn slewing(&self) -> ASCOMResult<bool> {
-        self.sa.is_slewing().await
+        self.is_slewing().await
     }
 
     async fn slew_settle_time(&self) -> ASCOMResult<i32> {
-        self.sa.get_slew_settle_time().await.map(|x| x as i32)
+        self.get_slew_settle_time().await.map(|x| x as i32)
     }
 
     async fn set_slew_settle_time(&self, slew_settle_time: i32) -> ASCOMResult<()> {
@@ -335,43 +327,42 @@ impl Telescope for AlpacaState {
                 "Slew settle time must be nonegative".to_string(),
             ));
         }
-        self.sa.set_slew_settle_time(slew_settle_time as u32).await
+        self.set_slew_settle_time(slew_settle_time as u32).await
     }
 
     async fn target_declination(&self) -> ASCOMResult<f64> {
-        self.sa.get_target_declination().await
+        self.get_target_declination().await
     }
 
     async fn set_target_declination(&self, target_declination: f64) -> ASCOMResult<()> {
-        self.sa.set_target_dec(target_declination).await
+        self.set_target_dec(target_declination).await
     }
 
     async fn target_right_ascension(&self) -> ASCOMResult<f64> {
-        self.sa.get_target_ra().await
+        self.get_target_ra().await
     }
 
     async fn set_target_right_ascension(&self, target_right_ascension: f64) -> ASCOMResult<()> {
-        self.sa.set_target_ra(target_right_ascension).await
+        self.set_target_ra(target_right_ascension).await
     }
 
     async fn tracking(&self) -> ASCOMResult<bool> {
-        self.sa.is_tracking().await
+        self.is_tracking().await
     }
 
     async fn set_tracking(&self, tracking: bool) -> ASCOMResult<()> {
-        self.sa.set_is_tracking(tracking).await
+        self.set_is_tracking(tracking).await
     }
 
     async fn tracking_rate(&self) -> ASCOMResult<i32> {
-        self.sa
-            .get_tracking_rate()
+        self.get_tracking_rate()
             .await
             .map(|drive_rate| drive_rate.into())
     }
 
     async fn set_tracking_rate(&self, tracking_rate: DriveRate) -> ASCOMResult<()> {
         match DriveRate::try_from(tracking_rate) {
-            Ok(r) => self.sa.set_tracking_rate(r).await,
+            Ok(r) => self.set_tracking_rate(r).await,
             Err(_e) => Err(ASCOMError::new(
                 ASCOMErrorCode::INVALID_VALUE,
                 "Invalid Tracking Rate".to_string(),
@@ -380,12 +371,11 @@ impl Telescope for AlpacaState {
     }
 
     async fn tracking_rates(&self) -> ASCOMResult<Vec<DriveRate>> {
-        self.sa.get_tracking_rates().await
+        self.get_tracking_rates().await
     }
 
     async fn utcdate(&self) -> ASCOMResult<String> {
-        self.sa
-            .get_utc_date()
+        self.get_utc_date()
             .await
             .map(|d| d.format(ALPACA_DATE_FMT).to_string())
     }
@@ -407,19 +397,19 @@ impl Telescope for AlpacaState {
             }
         };
 
-        self.sa.set_utc_date(d).await
+        self.set_utc_date(d).await
     }
 
     async fn abort_slew(&self) -> ASCOMResult<()> {
-        self.sa.abort_slew().await
+        self.abort_slew().await
     }
 
     async fn axis_rates(&self, axis: Axis) -> ASCOMResult<Vec<AxisRate>> {
-        self.sa.get_axis_rates(axis).await
+        self.get_axis_rates(axis).await
     }
 
     async fn can_move_axis(&self, axis: Axis) -> ASCOMResult<bool> {
-        self.sa.can_move_axis(axis).await
+        self.can_move_axis(axis).await
     }
 
     async fn destination_side_of_pier(
@@ -427,22 +417,21 @@ impl Telescope for AlpacaState {
         right_ascension: f64,
         declination: f64,
     ) -> ASCOMResult<SideOfPierResponse> {
-        self.sa
-            .predict_destination_side_of_pier(right_ascension, declination)
+        self.predict_destination_side_of_pier(right_ascension, declination)
             .await
     }
 
     async fn find_home(&self) -> ASCOMResult<()> {
-        self.sa.find_home().await
+        self.find_home().await
     }
 
     async fn move_axis(&self, axis: Axis, rate: f64) -> ASCOMResult<()> {
-        let result = self.sa.move_axis(axis, rate).await;
+        let result = self.move_axis(axis, rate).await;
         result
     }
 
     async fn park(&self) -> ASCOMResult<()> {
-        self.sa.park().await
+        self.park().await
     }
 
     async fn pulse_guide(
@@ -450,26 +439,25 @@ impl Telescope for AlpacaState {
         direction: PutPulseGuideDirection,
         duration: i32,
     ) -> ASCOMResult<()> {
-        self.sa.pulse_guide(direction, duration as u32).await
+        self.pulse_guide(direction, duration as u32).await
     }
 
     async fn set_park(&self) -> ASCOMResult<()> {
-        self.sa.set_park_pos().await
+        self.set_park_pos().await
     }
 
     async fn slew_to_alt_az(&self, azimuth: f64, altitude: f64) -> ASCOMResult<()> {
-        self.sa.slew_to_alt_az(altitude, azimuth).await?;
+        self.slew_to_alt_az(altitude, azimuth).await?;
         Ok(())
     }
 
     async fn slew_to_alt_az_async(&self, azimuth: f64, altitude: f64) -> ASCOMResult<()> {
-        let _finish = self.sa.slew_to_alt_az_async(altitude, azimuth).await?;
+        let _finish = self.slew_to_alt_az_async(altitude, azimuth).await?;
         Ok(())
     }
 
     async fn slew_to_coordinates(&self, right_ascension: f64, declination: f64) -> ASCOMResult<()> {
-        self.sa
-            .slew_to_coordinates(right_ascension, declination)
+        self.slew_to_coordinates(right_ascension, declination)
             .await?;
         Ok(())
     }
@@ -480,37 +468,34 @@ impl Telescope for AlpacaState {
         declination: f64,
     ) -> ASCOMResult<()> {
         let _finish = self
-            .sa
             .slew_to_coordinates_async(right_ascension, declination)
             .await?;
         Ok(())
     }
 
     async fn slew_to_target(&self) -> ASCOMResult<()> {
-        self.sa.slew_to_target().await?;
+        self.slew_to_target().await?;
         Ok(())
     }
 
     async fn slew_to_target_async(&self) -> ASCOMResult<()> {
-        let _finish = self.sa.slew_to_target_async().await?;
+        let _finish = self.slew_to_target_async().await?;
         Ok(())
     }
 
     async fn sync_to_alt_az(&self, azimuth: f64, altitude: f64) -> ASCOMResult<()> {
-        self.sa.sync_to_alt_az(altitude, azimuth).await
+        self.sync_to_alt_az(altitude, azimuth).await
     }
 
     async fn sync_to_coordinates(&self, right_ascension: f64, declination: f64) -> ASCOMResult<()> {
-        self.sa
-            .sync_to_coordinates(right_ascension, declination)
-            .await
+        self.sync_to_coordinates(right_ascension, declination).await
     }
 
     async fn sync_to_target(&self) -> ASCOMResult<()> {
-        self.sa.sync_to_target().await
+        self.sync_to_target().await
     }
 
     async fn unpark(&self) -> ASCOMResult<()> {
-        self.sa.unpark().await
+        self.unpark().await
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ mod consts;
 mod telescope_control;
 mod util;
 
-use alpaca_state::AlpacaState;
 use ascom_alpaca::api::CargoServerInfo;
 use ascom_alpaca::Server;
 use config::Config;
-use std::sync::atomic::AtomicU32;
 use telescope_control::StarAdventurer;
 use util::*;
 
@@ -18,16 +16,13 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let config = confy::load_path("config.toml").expect("Couldn't parse configuration");
-    let state = AlpacaState {
-        sa: StarAdventurer::new(&config).await,
-        sti: AtomicU32::new(0),
-    };
+    let sa = StarAdventurer::new(&config).await;
 
     let mut server = Server {
         info: CargoServerInfo!(),
         ..Default::default()
     };
-    server.devices.register(state);
+    server.devices.register(sa);
 
     server
         .listen_addr
